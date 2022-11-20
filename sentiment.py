a82dedbb125d6af87c1f81151a0df4df2532fe0f
alibaba0507 <alibaba0507@gmail.com>
2022-11-20 15:32:14 +0200

    init

---
+++ sentiment.py
@@ -0,0 +1,38 @@
+# import SentimentIntensityAnalyzer class
+# from vaderSentiment.vaderSentiment module.
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+class Sentiment:
+    def __init__(self) -> None:
+        return
+    # function to print sentiments
+    # of the sentence.
+    def sentiment_scores(self,sentence):
+     
+        # Create a SentimentIntensityAnalyzer object.
+        sid_obj = SentimentIntensityAnalyzer()
+     
+        # polarity_scores method of SentimentIntensityAnalyzer
+        # object gives a sentiment dictionary.
+        # which contains pos, neg, neu, and compound scores.
+        sentiment_dict = sid_obj.polarity_scores(sentence)
+         
+        print("Overall sentiment dictionary is : ", sentiment_dict)
+        print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+        print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+        print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+     
+        print("Sentence Overall Rated As", end = " ")
+     
+        # decide sentiment as positive, negative and neutral
+        if sentiment_dict['compound'] >= 0.05 :
+            print("Positive")
+     
+        elif sentiment_dict['compound'] <= - 0.05 :
+            print("Negative")
+     
+        else :
+            print("Neutral")
+ 
+ 
--- test_rss.py
+++ test_rss.py
@@ -1,6 +1,9 @@
 import pprint
+import spacy
 from rss_helper import RSSHelper
 from html_parser import HTML_Parser
+from sentiment import Sentiment
+#import en_core_web_sm
 
 rss_feed = 'https://finance.yahoo.com/news/rssindex'
 
@@ -12,4 +15,8 @@ parser = HTML_Parser()
  This range [0:1] is just for test
 '''
 text_content = [parser.extract_text_from_single_web_page(url) for url in links[0:1]]
-pprint.pprint(text_content[0])
+cleaned_textual_content = [text for text in text_content if str(text) != 'nan']
+
+s  = Sentiment()
+[s.sentiment_scores(t) for t in cleaned_textual_content]
+pprint.pprint(cleaned_textual_content[0])
